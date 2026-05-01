a140b92d4499277247bb81f8d7ee5e9b3b8b38a0
Dara Sen <darasen0277@gmail.com>
2026-05-01 12:20:30 -0700

    Add HomeInformationSection component and stories

---
+++ src/stories/HomeInformationSection.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeInformationSection from './HomeInformationSection';
+
+export default {
+    title: 'HomeInformationSection',
+    component: HomeInformationSection
+};
+
+export const Default = {
+    args: {
+        title: "Do you need IT help?",
+        subtitle: "We can help you and you can help us.",
+        description: "We have budding IT students, eager to learn and build real world development skills. And you are an organization who needs IT development to improve and expand your org."
+    }
+};
